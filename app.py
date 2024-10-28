0b3f10940310bc87c1486c38f1a318b42fc3d9e5
Temesgen Mesfin Tewolde <codeteme@gmail.com>
2024-10-28 12:29:07 +0300

    Add environment-based DB config for Azure and local development. Update app.py to load dev/prod configs. Ran db migration."

--- app.py
+++ app.py
@@ -5,19 +5,23 @@ from flask_migrate import Migrate
 from models import db
 from routes import main  # Import the routes blueprint
 
-# Load environment variables from .env file
-load_dotenv()
 # Initialize Flask app
 app = Flask(__name__)
 
-# Set the secret key to a random value or read it from an environment variable for security
-app.config['SECRET_KEY'] = os.urandom(24)
+# WEBSITE_HOSTNAME exists only in production environment
+if 'WEBSITE_HOSTNAME' not in os.environ:
+    # local development, where we'll use environment variables
+    print("Loading config.development and environment variables from .env file.")
+    app.config.from_object('azureproject.development')
+else:
+    # production
+    print("Loading config.production.")
+    app.config.from_object('azureproject.production')
 
-# PostgreSQL database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
 )
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database and migrations
 db.init_app(app)
