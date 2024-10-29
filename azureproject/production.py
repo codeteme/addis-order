5795c8db5641bb2998425d766cd58adfa0431491
Temesgen Mesfin Tewolde <codeteme@gmail.com>
2024-10-29 20:43:02 +0300

    minor bug fix in production.py print statement; bug fix in env var

--- azureproject/production.py
+++ azureproject/production.py
@@ -9,10 +9,13 @@ print("conn_str is not empty")
 
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
-print(conn_str_params['user'], 
-      dbpass=conn_str_params['password'], 
-      dbhost=conn_str_params['host'], 
-      dbname=conn_str_params['dbname'])
+print("conn_str_params: ", conn_str_params)
+
+print(conn_str_params['user'])
+print(conn_str_params['password'])
+print(conn_str_params['host'])
+print(conn_str_params['dbname'])
+print(conn_str_params['sslmode'])
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=conn_str_params['user'],
